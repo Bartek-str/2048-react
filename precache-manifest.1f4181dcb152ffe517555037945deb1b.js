69de2515afc08063da6c9f1bb56643a76fb59d61
Bartek-str <bartlomiejstruzyk@gmail.com>
2020-05-25 21:00:00 +0200

    Updates

---
+++ precache-manifest.1f4181dcb152ffe517555037945deb1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b77226beb8e132d3f6f4ceabc4dd386",
+    "url": "/2048-react/index.html"
+  },
+  {
+    "revision": "336c8a4c1a9ef53e433a",
+    "url": "/2048-react/static/css/main.cd28a4b2.chunk.css"
+  },
+  {
+    "revision": "48758a7230a689409fa8",
+    "url": "/2048-react/static/js/2.599d69bc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/2048-react/static/js/2.599d69bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "336c8a4c1a9ef53e433a",
+    "url": "/2048-react/static/js/main.9e006d89.chunk.js"
+  },
+  {
+    "revision": "f76cbb79b2d2d86ea8b4",
+    "url": "/2048-react/static/js/runtime-main.24253804.js"
+  }
+]);
